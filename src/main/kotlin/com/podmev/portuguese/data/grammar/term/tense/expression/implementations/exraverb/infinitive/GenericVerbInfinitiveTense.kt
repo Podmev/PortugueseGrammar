1e27ad16cdc84af5472d6b0e468d94446fe7d390
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-10-22 17:53:24 +0300

    GenericVerbInfinitiveTense

---
+++ src/main/kotlin/com/podmev/portuguese/data/grammar/term/tense/expression/implementations/exraverb/infinitive/GenericVerbInfinitiveTense.kt
@@ -0,0 +1,10 @@
+package com.podmev.portuguese.data.grammar.term.tense.expression.implementations.exraverb.infinitive
+
+import com.podmev.portuguese.data.grammar.term.tense.GrammaticalTense
+
+data class GenericVerbInfinitiveTense(
+    override val auxiliarVerb: String,
+    override val auxiliarVerbTense: GrammaticalTense
+): AbstractVerbInfinitiveTense{
+
+}
