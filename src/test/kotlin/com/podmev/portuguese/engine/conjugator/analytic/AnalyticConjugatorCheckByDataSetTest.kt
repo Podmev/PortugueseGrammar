0725b4e8303fb8de4d9b9b378219e0dea8095da6
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-10-24 02:00:24 +0300

    created empty AnalyticConjugatorCheckByDataSetTest

---
+++ src/test/kotlin/com/podmev/portuguese/engine/conjugator/analytic/AnalyticConjugatorCheckByDataSetTest.kt
@@ -0,0 +1,14 @@
+package com.podmev.portuguese.engine.conjugator.analytic
+
+import com.podmev.portuguese.engine.conjugator.dataset.only.OnlyDataSetConjugator
+import com.podmev.portuguese.engine.dataset.verb.getAllVerbs
+import org.junit.Test
+
+class AnalyticConjugatorCheckByDataSetTest {
+//    @Test
+//    fun allVerbTest(){
+//        val allVerbs = getAllVerbs()
+//        val coveringTenses = OnlyDataSetConjugator.getCoveredTenses()
+//        val coveringVoices = listOf<>()
+//    }
+}
