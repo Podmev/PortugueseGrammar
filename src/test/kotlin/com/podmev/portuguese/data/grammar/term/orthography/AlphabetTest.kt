10b8fb5cb76cf213c473e6aa05c3c76c5e8721fb
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-10-28 02:13:02 +0300

    upper and lowercase tests

--- src/test/kotlin/com/podmev/portuguese/data/grammar/term/orthography/AlphabetTest.kt
+++ src/test/kotlin/com/podmev/portuguese/data/grammar/term/orthography/AlphabetTest.kt
@@ -1,8 +1,13 @@
 package com.podmev.portuguese.data.grammar.term.orthography
 
+import com.google.common.truth.Truth
 import com.podmev.portuguese.data.grammar.term.orthography.letters.*
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class AlphabetTest {
     @Test
@@ -63,6 +68,26 @@ class AlphabetTest {
             /* actual = */ consonants,
             /* message = */ "all consonants"
         )
+    }
+
+    @ParameterizedTest()
+    @MethodSource("letterProvider")
+    fun uppercaseTest(letter: Letter) {
+        val firstLetterOfClassName = letter.javaClass.simpleName[0]
+        Truth.assertThat(firstLetterOfClassName).isEqualTo(letter.uppercase)
+    }
+
+    @ParameterizedTest()
+    @MethodSource("letterProvider")
+    fun upperAndLowercaseCorrespondenceTest(letter: Letter) {
+        Truth.assertThat(letter.uppercase.toLowerCase()).isEqualTo(letter.lowercase)
+    }
+
+    companion object {
+        @JvmStatic
+        fun letterProvider(): Stream<Arguments> =
+            Alphabet.letters.stream().map{letter->Arguments.of(letter)}
+
     }
 
 }
