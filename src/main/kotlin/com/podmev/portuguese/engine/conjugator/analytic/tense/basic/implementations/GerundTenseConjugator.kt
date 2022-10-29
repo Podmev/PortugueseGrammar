d1b19bd6a8b3f22bc81789bc92e0cc7505280e7a
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-10-30 00:56:18 +0300

    finished GerundTenseConjugator

--- src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/tense/basic/implementations/GerundTenseConjugator.kt
+++ src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/tense/basic/implementations/GerundTenseConjugator.kt
@@ -3,6 +3,8 @@ package com.podmev.portuguese.engine.conjugator.analytic.tense.basic.implementat
 import com.podmev.portuguese.data.grammar.term.tense.GrammaticalTense
 import com.podmev.portuguese.data.grammar.term.verb.VerbArguments
 import com.podmev.portuguese.engine.conjugator.analytic.tense.basic.BasicTenseConjugator
+import com.podmev.portuguese.engine.utils.word.VerbEnds
+import com.podmev.portuguese.engine.utils.word.Wordifier
 
 object GerundTenseConjugator : BasicTenseConjugator {
     override fun conjugateVerb(
@@ -10,10 +12,25 @@ object GerundTenseConjugator : BasicTenseConjugator {
         tense: GrammaticalTense,
         verbArgs: VerbArguments
     ): List<String> {
-        return listOf(regularChanging(verbInInfinitive))
+        val preparedInfinitive = prepareInfinitive(verbInInfinitive)
+        val preparedBase = takeInfinitiveSuffix(preparedInfinitive)
+        val result = regularChanging(preparedBase)
+        return listOf(result)
     }
 
-    private fun regularChanging(verbInInfinitive: String):String = verbInInfinitive.dropLast(1) + "ndo"
+    const val regularSuffix = "ndo"
+
+    private fun regularChanging(preparedBase: String):String = preparedBase + regularSuffix
+    /*drop the last letter 'r'*/
+    private fun takeInfinitiveSuffix(infinitive: String):String = infinitive.dropLast(1)
+
+    private fun prepareInfinitive(originalInfinitive: String): String {
+        if(originalInfinitive.endsWith(VerbEnds.O_CIRCUMFLEX_R)){
+            return Wordifier.deleteLastDiacritics(originalInfinitive)
+        }
+        //the most of the cases we don't change anything
+        return originalInfinitive
+    }
 
     override fun toString(): String {
         return "GerundTenseConjugator"
---
+++ src/main/kotlin/com/podmev/portuguese/engine/utils/word/VerbEnds.kt
@@ -0,0 +1,16 @@
+package com.podmev.portuguese.engine.utils.word
+
+object VerbEnds {
+    const val AR = "ar"
+    const val ER = "er"
+    const val IR = "ir"
+
+    const val UIR = "uir"
+    const val AIR = "air"
+
+    const val GUIR = "guir"
+    const val QUIR = "quir"
+
+    const val O_CIRCUMFLEX_R = "ôr"
+
+}
