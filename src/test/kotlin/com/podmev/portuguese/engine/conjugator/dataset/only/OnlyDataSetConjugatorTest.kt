f23b45fde7b487b063da07fa213ecb803408162d
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-10-30 16:57:56 +0300

    OnlyDataSetConjugatorTest: past participle and one for ser/estar

--- src/test/kotlin/com/podmev/portuguese/engine/conjugator/dataset/only/OnlyDataSetConjugatorTest.kt
+++ src/test/kotlin/com/podmev/portuguese/engine/conjugator/dataset/only/OnlyDataSetConjugatorTest.kt
@@ -5,10 +5,13 @@ import com.podmev.portuguese.data.grammar.term.general.GrammaticalNumber
 import com.podmev.portuguese.data.grammar.term.general.GrammaticalPerson
 import com.podmev.portuguese.data.grammar.term.tense.basic.implementations.GerundTense
 import com.podmev.portuguese.data.grammar.term.tense.basic.implementations.InfinitiveTense
+import com.podmev.portuguese.data.grammar.term.tense.basic.implementations.PastParticipleForSerEstarTense
+import com.podmev.portuguese.data.grammar.term.tense.basic.implementations.PastParticipleTense
 import com.podmev.portuguese.data.grammar.term.tense.basic.implementations.indicative.IndicativeImperfectTense
 import com.podmev.portuguese.data.grammar.term.verb.GrammaticalVoice
 import com.podmev.portuguese.data.grammar.term.verb.VerbArguments
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class OnlyDataSetConjugatorTest {
@@ -70,4 +73,315 @@ class OnlyDataSetConjugatorTest {
             /* message = */ "Verb 'fazer' in gerund"
         )
     }
+
+    @Nested
+    inner class PastParticiple{
+        @Test
+        fun falarMasculineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("falado"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle masculine singular"
+            )
+        }
+
+        @Test
+        fun falarFeminineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("falada"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle feminine singular"
+            )
+        }
+
+        @Test
+        fun falarMasculinePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("falados"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle masculine plural"
+            )
+        }
+
+        @Test
+        fun falarFemininePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("faladas"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle feminine plural"
+            )
+        }
+
+        @Test
+        fun pagarMasculineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pagado"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle masculine singular"
+            )
+        }
+
+        @Test
+        fun pagarFeminineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pagada"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle feminine singular"
+            )
+        }
+
+        @Test
+        fun pagarMasculinePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pagados"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle masculine plural"
+            )
+        }
+
+        @Test
+        fun pagarFemininePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pagadas"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle feminine plural"
+            )
+        }
+    }
+
+    @Nested
+    inner class PastParticipleSerEstar{
+        @Test
+        fun falarMasculineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("falado"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle for ser/estar masculine singular"
+            )
+        }
+
+        @Test
+        fun falarFeminineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("falada"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle for ser/estar feminine singular"
+            )
+        }
+
+        @Test
+        fun falarMasculinePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("falados"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle for ser/estar masculine plural"
+            )
+        }
+
+        @Test
+        fun falarFemininePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "falar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("faladas"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'falar' in past participle for ser/estar feminine plural"
+            )
+        }
+
+        @Test
+        fun pagarMasculineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pago"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle for ser/estar masculine singular"
+            )
+        }
+
+        @Test
+        fun pagarFeminineSingularTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.SINGULAR,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("paga"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle for ser/estar feminine singular"
+            )
+        }
+
+        @Test
+        fun pagarMasculinePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.MASCULINE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pagos"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle for ser/estar masculine plural"
+            )
+        }
+
+        @Test
+        fun pagarFemininePluralTest() {
+            val verbInForm: List<String> = conjugator.conjugateVerb(
+                verbInInfinitive = "pagar",
+                tense = PastParticipleForSerEstarTense,
+                verbArgs = VerbArguments(
+                    person = GrammaticalPerson.UNDEFINED,
+                    number = GrammaticalNumber.PLURAL,
+                    gender = GrammaticalGender.FEMININE,
+                    voice = GrammaticalVoice.ACTIVE
+                )
+            )
+            Assertions.assertEquals(
+                /* expected = */ listOf("pagas"),
+                /* actual = */ verbInForm,
+                /* message = */ "Verb 'pagar' in past participle for ser/estar feminine plural"
+            )
+        }
+    }
+
 }
