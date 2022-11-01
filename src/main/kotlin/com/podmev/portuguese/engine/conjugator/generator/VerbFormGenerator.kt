06d0de5fc83aaa5bd2e5ba0083b2eec49036372e
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-11-01 15:43:20 +0300

    VerbFormGenerator: demo code

--- src/main/kotlin/com/podmev/portuguese/engine/conjugator/generator/VerbFormGenerator.kt
+++ src/main/kotlin/com/podmev/portuguese/engine/conjugator/generator/VerbFormGenerator.kt
@@ -1,23 +1,28 @@
 package com.podmev.portuguese.engine.conjugator.generator
 
 import com.podmev.portuguese.data.engine.conjugator.Conjugator
-import com.podmev.portuguese.data.grammar.term.tense.basic.implementations.PastParticipleTense
 import com.podmev.portuguese.data.grammar.term.verb.VerbFormInfo
-import com.podmev.portuguese.engine.conjugator.analytic.AnalyticConjugator
-import com.podmev.portuguese.engine.conjugator.dataset.only.OnlyDataSetConjugator
+import com.podmev.portuguese.engine.conjugator.analytic.tense.basic.implementations.PastParticipleTenseConjugator
+import com.podmev.portuguese.engine.utils.word.Wordifier
 
 object VerbFormGenerator {
 
     fun pastParticipleHelper(){
-        val wrongMatches = getWrongMatches(
-            etalonConjugator = OnlyDataSetConjugator,
-            checkingConjugator = AnalyticConjugator,
-            verbForms = OnlyDataSetConjugator.getConjugatorCoveringDataWithFixedTense(PastParticipleTense).getVerbFormSingularMasculineNoPersonInfos()
-        )
-        //TODO add sort by ends
-        for (match in wrongMatches.distinct()){
-            println("Pair(\"${match.form.infinitive}\", \"${match.correctForm.first()}\"),")
+//        val wrongMatches = getWrongMatches(
+//            etalonConjugator = OnlyDataSetConjugator,
+//            checkingConjugator = AnalyticConjugator,
+//            verbForms = OnlyDataSetConjugator.getConjugatorCoveringDataWithFixedTense(PastParticipleTense).getAllVerbFormInfos()
+//        )
+//        //TODO add sort by ends
+//        for (match in wrongMatches.distinct()){
+//            println("\"${match.form.infinitive}\", \"${match.correctForm.first()}\", \"${match.wrongForm.first()}\"")
+//        }
+        Wordifier.groupByEndOfWords(PastParticipleTenseConjugator.fullIrregularFormMap.keys.toList()).forEach {
+            println(it)
         }
+//        for (match in wrongMatches.distinct()){
+//            println("Pair(\"${match.form.infinitive}\", \"${match.correctForm.first()}\"),")
+//        }
     }
 
     fun getWrongMatches(
