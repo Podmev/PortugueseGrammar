adb1369fbcf2a4b02133a2e0935366fa1766fffa
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-11-02 19:36:20 +0300

    IndicativePresentTenseConjugator add construir, destruir base rule: 1460 fails / 24500

--- src/main/kotlin/com/podmev/portuguese/data/grammar/term/verb/VerbArguments.kt
+++ src/main/kotlin/com/podmev/portuguese/data/grammar/term/verb/VerbArguments.kt
@@ -17,3 +17,33 @@ data class VerbArguments(
 
 fun VerbArguments.isFirstSingular(): Boolean =
     person == GrammaticalPerson.FIRST && number == GrammaticalNumber.SINGULAR
+
+fun VerbArguments.isSecondSingular(): Boolean =
+    person == GrammaticalPerson.SECOND && number == GrammaticalNumber.SINGULAR
+
+fun VerbArguments.isThirdSingular(): Boolean =
+    person == GrammaticalPerson.THIRD && number == GrammaticalNumber.SINGULAR
+
+fun VerbArguments.isFirstPlural(): Boolean =
+    person == GrammaticalPerson.FIRST && number == GrammaticalNumber.PLURAL
+
+fun VerbArguments.isSecondPlural(): Boolean =
+    person == GrammaticalPerson.SECOND && number == GrammaticalNumber.PLURAL
+
+fun VerbArguments.isThirdPlural(): Boolean =
+    person == GrammaticalPerson.THIRD && number == GrammaticalNumber.PLURAL
+
+fun VerbArguments.isFirst(): Boolean =
+    person == GrammaticalPerson.FIRST
+
+fun VerbArguments.isSecond(): Boolean =
+    person == GrammaticalPerson.SECOND
+
+fun VerbArguments.isThird(): Boolean =
+    person == GrammaticalPerson.THIRD
+
+fun VerbArguments.isSingular(): Boolean =
+    number == GrammaticalNumber.SINGULAR
+
+fun VerbArguments.isPlural(): Boolean =
+    number == GrammaticalNumber.PLURAL
--- src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/tense/basic/implementations/indicative/IndicativePresentTenseConjugator.kt
+++ src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/tense/basic/implementations/indicative/IndicativePresentTenseConjugator.kt
@@ -1,8 +1,10 @@
 package com.podmev.portuguese.engine.conjugator.analytic.tense.basic.implementations.indicative
 
 import com.podmev.portuguese.data.engine.conjugator.SuffixGroup
-import com.podmev.portuguese.data.grammar.term.verb.VerbArguments
-import com.podmev.portuguese.data.grammar.term.verb.isFirstSingular
+import com.podmev.portuguese.data.grammar.term.orthography.diacriticLetters.acute.O_Acute_Letter
+import com.podmev.portuguese.data.grammar.term.orthography.letters.O_Letter
+import com.podmev.portuguese.data.grammar.term.orthography.letters.U_Letter
+import com.podmev.portuguese.data.grammar.term.verb.*
 import com.podmev.portuguese.engine.conjugator.analytic.BaseChangingRule
 import com.podmev.portuguese.engine.conjugator.analytic.FiniteTenseConjugator
 import com.podmev.portuguese.engine.conjugator.analytic.SpecialEndingSuffixRule
@@ -11,6 +13,7 @@ import com.podmev.portuguese.engine.conjugator.analytic.VerbHelper.replaceIfNece
 import com.podmev.portuguese.engine.conjugator.analytic.VerbHelper.replaceIfNecessaryE_LetterForI_LetterOrNull
 import com.podmev.portuguese.engine.conjugator.analytic.VerbHelper.replaceIfNecessaryG_LetterForJ_LetterOrNull
 import com.podmev.portuguese.engine.utils.verb.VerbEnds
+import com.podmev.portuguese.engine.utils.word.Wordifier
 
 object IndicativePresentTenseConjugator : IndicativeMoodTenseConjugator, FiniteTenseConjugator {
     override val arSuffix = SuffixGroup("o", "as", "a", "amos", "ais", "am")
@@ -22,16 +25,19 @@ object IndicativePresentTenseConjugator : IndicativeMoodTenseConjugator, FiniteT
         override fun getSuffix(verb: String, regularSuffix: SuffixGroup) =
             regularSuffix.copy(singularThird = "") //finishes with -z
     }
+
     object AIR_Suffix_Rule : SpecialEndingSuffixRule {
         override val wordEnding = VerbEnds.AIR
         override fun getSuffix(verb: String, regularSuffix: SuffixGroup) =
             SuffixGroup("io", "is", "i", "ímos", "ís", regularSuffix.pluralThird)
     }
+
     object UIR_Suffix_Rule : SpecialEndingSuffixRule {
         override val wordEnding = VerbEnds.UIR
         override fun getSuffix(verb: String, regularSuffix: SuffixGroup) =
             SuffixGroup(regularSuffix.singularFirst, "is", "i", "ímos", "ís", regularSuffix.pluralThird)
     }
+
     override val specialEndingSuffixRules: List<SpecialEndingSuffixRule> = listOf(
         UZIR_Suffix_Rule,
         AIR_Suffix_Rule,
@@ -62,11 +68,27 @@ object IndicativePresentTenseConjugator : IndicativeMoodTenseConjugator, FiniteT
             replaceIfNecessaryE_LetterForI_LetterOrNull(verb)
     }
 
+    object Construir_Destruir_U_TO_O_Rule : BaseChangingRule {
+        override fun isCorrectForm(verbArgs: VerbArguments): Boolean = verbArgs.isSecondSingular() || verbArgs.isThird()
+        override fun changeBaseIfPossible(verb: String, exactSuffix: String, verbArgs: VerbArguments): String? =
+            if (verb in listOf("construir", "destruir"))
+                Wordifier.replaceLastFoundGenericLetter(
+                    word = verb,
+                    fromGenericLetter = U_Letter,
+                    toGenericLetter =
+                    if (verbArgs.isThirdPlural())
+                        O_Letter
+                    else O_Acute_Letter //second-singular and third-singular
+                )
+            else null
+    }
+
     override val baseChangingRules = listOf(
         C_TO_C_Cedilla_Rule,
         G_TO_J_Rule,
         EGU_TO_IG_Rule,
 //        E_TO_I_Rule - excluded for now
+        Construir_Destruir_U_TO_O_Rule
     )
 
     override fun toString(): String {
