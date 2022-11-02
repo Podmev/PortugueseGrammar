9c9d497713ff720a820c96c67736dedebcb41e8f
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-11-02 21:49:44 +0300

    MostFrequentVerbs

---
+++ src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/MostFrequentVerbs.kt
@@ -0,0 +1,30 @@
+package com.podmev.portuguese.engine.conjugator.analytic
+
+object MostFrequentVerbs {
+    const val SER = "ser"
+    const val DIZER = "dizer"
+    const val TER = "ter"
+    const val IR = "ir"
+    const val ESTAR = "estar"
+    const val FAZER = "fazer"
+    const val HAVER = "haver"
+    const val PODER = "poder"
+    const val VER = "ver"
+    const val DAR = "dar"
+    const val SABER = "saber"
+    const val VIR = "vir"
+    const val QUERER = "querer"
+    const val PARECER = "parecer"
+    const val FALAR = "falar"
+    const val FICAR = "ficar"
+    const val OUVIR = "ouvir"
+    const val ACHAR = "achar"
+    const val DEIXAR = "deixar"
+    const val SAIR = "sair"
+    const val CHEGAR = "chegar"
+    const val PEDIR = "pedir"
+    const val LER = "ler"
+    const val ACABAR = "acabar"
+    const val POR = "pôr"
+
+}
