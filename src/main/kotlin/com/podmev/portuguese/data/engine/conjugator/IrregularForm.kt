c08429eb91565dade81e0dca966c4597ef585b0a
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-11-03 00:12:57 +0300

    IrregularForm.getForm

--- src/main/kotlin/com/podmev/portuguese/data/engine/conjugator/IrregularForm.kt
+++ src/main/kotlin/com/podmev/portuguese/data/engine/conjugator/IrregularForm.kt
@@ -1,9 +1,34 @@
 package com.podmev.portuguese.data.engine.conjugator
 
+import com.podmev.portuguese.data.grammar.term.verb.VerbArguments
+import com.podmev.portuguese.engine.conjugator.analytic.RegularTransformation
+
 data class IrregularForm(
     val formGroup: FormGroup? = null,
     //immutable
     val suffixGroup: SuffixGroup? = null,
     val base: String? = null
-)
+){
+    fun getForm(verbArguments: VerbArguments, regularTransformation: RegularTransformation?): String?{
+        //trying formGroup
+        val createdByFormGroup = formGroup?.getForm(verbArguments)
+        if(createdByFormGroup!=null){
+            return  createdByFormGroup
+        }
+        val irregularSuffix = suffixGroup?.getSuffix(verbArguments)
+        if(irregularSuffix==null){
+            if(base!=null && regularTransformation!=null){
+                return base + regularTransformation.suffix
+            }
+        } else {
+            if(base!=null){
+                return base + irregularSuffix
+            } else if(regularTransformation!=null){
+                return regularTransformation.base + irregularSuffix
+            }
+        }
+        //there is no irregular form
+        return null
+    }
+}
 
