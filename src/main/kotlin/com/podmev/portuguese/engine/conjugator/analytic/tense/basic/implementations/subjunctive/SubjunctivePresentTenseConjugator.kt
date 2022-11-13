e6b25ff50f4b187ef9b275b2341f7d335215642c
Dmitriy Pozdin <podmev@gmail.com>
2022-11-13 06:35:56 +0300

    first version of SubjunctivePresentTenseConjugator: only regular forms. failed 6051/27000

--- src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/tense/basic/implementations/subjunctive/SubjunctivePresentTenseConjugator.kt
+++ src/main/kotlin/com/podmev/portuguese/engine/conjugator/analytic/tense/basic/implementations/subjunctive/SubjunctivePresentTenseConjugator.kt
@@ -1,16 +1,19 @@
 package com.podmev.portuguese.engine.conjugator.analytic.tense.basic.implementations.subjunctive
 
-import com.podmev.portuguese.data.grammar.term.tense.GrammaticalTense
-import com.podmev.portuguese.data.grammar.term.verb.VerbArguments
+import com.podmev.portuguese.data.engine.conjugator.*
+import com.podmev.portuguese.engine.conjugator.analytic.FiniteTenseConjugator
 
-object SubjunctivePresentTenseConjugator : SubjunctiveMoodTenseConjugator {
-    override fun conjugateVerb(
-        verbInInfinitive: String,
-        tense: GrammaticalTense,
-        verbArgs: VerbArguments
-    ): List<String> {
-        TODO("Not yet implemented")
-    }
+object SubjunctivePresentTenseConjugator : SubjunctiveMoodTenseConjugator, FiniteTenseConjugator() {
+    override val arSuffix = SuffixGroup("e", "es", "e", "emos", "eis", "em")
+    override val erSuffix = SuffixGroup("a", "as", "a", "amos", "ais", "am")
+    override val irSuffix = erSuffix //it is exactly the same
+
+    override val specialEndingSuffixRules: List<SpecialEndingSuffixRule> = listOf()
+    override val baseChangingRules: List<BaseChangingRule> = listOf()
+    override val irregularForms: Map<String, IrregularForm> = mapOf()
+    override val currentDefectiveGroups: Map<DefectiveGroup, List<String>>
+        get() = mapOf()
+
 
     override fun toString(): String {
         return "SubjunctivePresentTenseConjugator"
